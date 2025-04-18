44b390d20d3c67c245248c75b728275390897411
Shanto Islam <ishanto412@gmail.com>
2025-04-18 19:00:23 +0600

    feat: pwa

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,31 +1,25 @@
-import { MetadataRoute } from "next";
-
-
-// const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "SonicSoal",
+    name: "Sonic Soal",
     short_name: "SonicSoal",
-    description:
-      "Experience the power of frequency-optimized audio designed to enhance focus, relaxation, and spiritual alignment.",
-    start_url: "/",
-    display: "standalone",
-    theme_color: "#ffffff",
-    background_color: "#ffffff",
+    description: "Experience the power of frequency-optimized audio designed to enhance focus, relaxation, and spiritual alignment.",
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
     icons: [
       {
-        src: "/manifest-icon-192.maskable.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
+        src: '/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
       {
-        src: "/manifest-icon-512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
+        src: '/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
-  };
+  }
 }
