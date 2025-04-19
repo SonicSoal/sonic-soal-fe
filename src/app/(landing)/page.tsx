32ad9d66a7bd11ddfd7e2463d5f6300e9ceefebb
Shanto Islam <ishanto412@gmail.com>
2025-04-19 17:35:14 +0600

    feat: session feedbackl

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,6 @@
 import { CTA } from '@/app/(landing)/_components/cta';
 import { EarnSoal } from '@/app/(landing)/_components/earn-soal';
 import FaqAccordion from '@/app/(landing)/_components/faq-section';
-import { Footer } from '@/app/(landing)/_components/footer';
-import { Header } from '@/app/(landing)/_components/header';
 import { Hero } from '@/app/(landing)/_components/hero';
 import { HowItWorks } from '@/app/(landing)/_components/how-it-works';
 import { VideoShowcase } from '@/app/(landing)/_components/video-showcase';
