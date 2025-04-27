bab0142f580398c12f47db8c844366cfede2e6a9
team-psl <team.onepsl@gmail.com>
2025-04-27 15:32:09 +0600

    fix: origin and feedback

--- next.config.ts
+++ next.config.ts
@@ -5,27 +5,6 @@ const nextConfig: NextConfig = {
   devIndicators:{
     position: "bottom-right"
   },
-  async headers() {
-    return [
-      {
-        source: '/.well-known/xrp-ledger.toml',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,POST,PUT,DELETE',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type,Authorization',
-          },
-        ],
-      },
-    ]
-  },
 };
 
 export default nextConfig;
