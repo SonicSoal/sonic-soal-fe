3c4404724b20bf35e7ad98124b49da5eb7495939
team-psl <team.onepsl@gmail.com>
2025-04-27 14:18:37 +0600

    fix: origin and feedback

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+// middleware.ts
+import { NextRequest, NextResponse } from 'next/server'
+
+const allowAllHeaders = new Headers({
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+})
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const method = request.method;
+
+  if (pathname === '/.well-known/xrp-ledger.toml') {
+
+    if (method === 'OPTIONS') {
+      return new NextResponse(null, {
+        status: 204,
+        headers: allowAllHeaders,
+      })
+    }
+
+    const res = NextResponse.next()
+    allowAllHeaders.forEach((value, key) => {
+      res.headers.set(key, value)
+    })
+    return res
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/.well-known/xrp-ledger.toml'],
+}
