9a846704a422ced2e7168e10756a76f294685867
team-psl <team.onepsl@gmail.com>
2025-04-27 13:37:50 +0600

    fix: origin and feedback

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,19 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators:{
     position: "bottom-right"
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin',   value: '*' },
+          { key: 'Access-Control-Allow-Methods',  value: 'GET,OPTIONS,POST,PUT,DELETE' },
+          { key: 'Access-Control-Allow-Headers',  value: 'Content-Type,Authorization' }
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
